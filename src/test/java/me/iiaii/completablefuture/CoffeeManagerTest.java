87cb92766e75725fdfd6af5bfdc3d561d4ad2b80
최정헌 <iiaii@zuminternet.com>
2020-08-07 09:11:22 +0900

    allOf 추가

--- src/test/java/me/iiaii/completablefuture/CoffeeManagerTest.java
+++ src/test/java/me/iiaii/completablefuture/CoffeeManagerTest.java
@@ -1,5 +1,6 @@
 package me.iiaii.completablefuture;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -8,9 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+import java.util.function.IntFunction;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,9 +44,13 @@ public class CoffeeManagerTest {
     @Autowired
     private CoffeeRepository coffeeRepository;
 
-
-
     Logger logger = LoggerFactory.getLogger(CoffeeManagerTest.class);
+
+    @BeforeEach
+    void init() {
+        logger.info("커피 레포 초기화");
+        coffeeRepository.deleteAll();
+    }
 
     @Test
     public void 커피가져오기_동기로직() throws Exception {
@@ -57,10 +67,13 @@ public class CoffeeManagerTest {
 
         // then
         assertEquals(coffee, findCoffee);
-
+        coffeeRepository.deleteAll();
     }
 
-
+    /**
+     * join (Blocking)
+     * @throws Exception
+     */
     @Test
     public void 커피가져오기_비동기로직_블로킹() throws Exception {
         logger.info("커피 가져오기 비동기 로직(블로킹) 시작");
@@ -81,12 +94,16 @@ public class CoffeeManagerTest {
 
         Coffee receivedCoffee = future.join();
         logger.info("커피 전달 받음 : "+ receivedCoffee);
-
+        // join 될 때까지 다른 작업 수행 불가능 (Blocking)
 
         // then
         assertEquals(coffee, receivedCoffee);
     }
 
+    /**
+     * thenApply, thenAccept (Non-Blocking)
+     * @throws Exception
+     */
     @Test
     public void 커피가져오기_비동기로직_콜백() throws Exception {
         logger.info("커피 가져오기 비동기 로직(콜백) 시작");
@@ -111,6 +128,7 @@ public class CoffeeManagerTest {
                     assertEquals(coffee, c);
                 });
 
+        // 다른 작업 수행 가능 (Non-Blocking)
         for (int i = 0; i <3 ; i++) {
             logger.info("다른 작업 수행 가능 논 블로킹 "+i);
             Thread.sleep(1000);
@@ -120,6 +138,10 @@ public class CoffeeManagerTest {
         assertNull(future.join());
     }
 
+    /**
+     * thenApply, thenAccept (Non-Blocking) 다른 스레드로 실행
+     * @throws Exception
+     */
     @Test
     public void 커피가져오기_비동기로직_콜백_다른스레드() throws Exception {
         logger.info("커피 가져오기 비동기 로직(콜백 다른스레드) 시작");
@@ -155,9 +177,13 @@ public class CoffeeManagerTest {
         assertNull(future.join());
     }
 
+    /**
+     * thenCombine (순차 실행 X)
+     * @throws Exception
+     */
     @Test
-    public void 커피가져오기_비동기로직_2개CompletableFuture조합() throws Exception {
-        logger.info("커피 가져오기 비동기 로직(2개 조합) 시작");
+    public void 할인커피가져오기_비동기로직_2개조합_순차없음() throws Exception {
+        logger.info("커피 가져오기 비동기 로직(2개 조합 순차 없음) 시작");
         // given
         Coffee coffee1 = Coffee.builder()
                 .name("coldBrew")
@@ -182,24 +208,83 @@ public class CoffeeManagerTest {
         assertEquals(expectedTotalPrice, totalPrice.intValue());
     }
 
-
+    /**
+     * thenCompose (순차 실행)
+     * @throws Exception
+     */
     @Test
-    public void 할인커피가져오기_비동기로직_2개조합() throws Exception {
-        logger.info("할인커피 가져오기 비동기 로직(2개 조합) 시작");
+    public void 할인커피가져오기_비동기로직_2개조합_순차실행() throws Exception {
+        logger.info("할인커피 가져오기 비동기 로직(2개 조합 순차 실행) 시작");
         // given
         Coffee coffee = Coffee.builder()
                 .name("coldBrew")
                 .price(5000)
                 .build();
         coffeeRepository.save(coffee);
+        int expectedPrice = coffee.getPrice() - 1000;
 
         CompletableFuture<Coffee> future = coffeeManager.getCoffeeAsync(coffee.getName());
 
         // when
-        future.thenCombine(c -> coffeeManager.getDiscountCoffeeAsync(c)).join();
+        Coffee discountedCoffee = future.thenCompose(c -> coffeeManager.getDiscountCoffeeAsync(c))
+                .join();
 
         // then
-        assertEquals(coffee.getPrice(), discountCoffee.getPrice());
+        assertEquals(expectedPrice, discountedCoffee.getPrice());
+    }
+
+
+    @Test
+    public void 여러개의_커피가져오기() throws Exception {
+        // given
+        List<Coffee> coffees = new ArrayList<>();
+
+        coffees.add(
+                Coffee.builder()
+                .name("coldBrew")
+                .price(5000)
+                .build()
+        );
+        coffees.add(
+                Coffee.builder()
+                        .name("latte")
+                        .price(6000)
+                        .build()
+        );
+        coffees.add(
+                Coffee.builder()
+                        .name("mocha")
+                        .price(5500)
+                        .build()
+        );
+        coffees.forEach(coffeeRepository::save);
+
+        int expectedPrice = coffees.stream()
+                .mapToInt(Coffee::getPrice)
+                .reduce(0, Integer::sum);
+
+        List<CompletableFuture<Coffee>> completableFutures = coffees.stream()
+                .map(Coffee::getName)
+                .map(coffeeManager::getCoffeeAsync)
+                .collect(Collectors.toList());
+
+        // when
+        List<Coffee> completedCoffees = CompletableFuture
+                .allOf(completableFutures.stream().toArray(CompletableFuture[]::new))
+                .thenApply(Void ->
+                        completableFutures.stream()
+                                .map(CompletableFuture::join)
+                                .collect(Collectors.toList())
+                )
+                .join();
+
+        int totalPrice = completedCoffees.stream()
+                .mapToInt(Coffee::getPrice)
+                .reduce(0, Integer::sum);
+
+
+        // then
+        assertEquals(expectedPrice, totalPrice);
     }
 
 }
